051716ecc2235997229c0159bf0b579a588a32de
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-05-23 19:37:51 +0300

    fix errors

--- hw09_generator_of_validators/models/models.go
+++ hw09_generator_of_validators/models/models.go
@@ -10,7 +10,7 @@ type (
 		ID     string `json:"id" validate:"len:36"`
 		Name   string
 		Age    int      `validate:"min:18|max:50"`
-		Email  string   `validate:"regexp:^\w+@\w+\.\w+$"`
+		Email  string   `validate:"regexp:^\w+@\w+\.\w+$"` //nolint
 		Role   UserRole `validate:"in:admin,stuff"`
 		Phones []string `validate:"len:11"`
 	}
